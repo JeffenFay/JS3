a897554318cd18978dc6f93711f5b76fb0206e3a
JeffenFay <jeffen.fay@gmail.com>
2018-10-08 11:57:12 +0200

    Regex enlightmentent git add .!

--- javascript/index3js.js
+++ javascript/index3js.js
@@ -1,11 +1,13 @@
 var firstname = prompt("Tape ton prénom, stp :");
 function verifName(saisie){
-     var regex = new RegExp("[a-zA-Z-éèùÿïç]","g");
-   if(!regex.test(saisie)|saisie==null){
+     const regex =  /^[a-zA-ZäãåçéèêëíìîïñóòôöõúùûüÿÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜŸ-]+$/;
+   if(!regex.test(saisie)||saisie==null){
+     //PAS BON
      alert("C'est pas un prénom ça !");
-     var saisie2 = prompt("Tape encore ton prénom, stp :");
-     verifName(saisie2);
+     saisie = prompt("Tape encore ton prénom, stp :");
+     verifName(saisie );
    }else{
+     //BON
      alert("Bonjour, "+saisie+" !");
    }
 };
