291de26a8d88c6bfb3044bab04ea6a87abf65160
JeffenFay <jeffen.fay@gmail.com>
2018-10-08 17:02:36 +0200

    Commentaires

--- javascript/index3js.js
+++ javascript/index3js.js
@@ -1,14 +1,18 @@
 var firstname = prompt("Tape ton prénom, stp :");
+//Fonction qui vérifie et affiche le résultat
 function verifName(saisie){
-     const regex =  /^[a-zA-ZäãåçéèêëíìîïñóòôöõúùûüÿÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜŸ-]+$/;
-   if(!regex.test(saisie)||saisie==null){
-     //PAS BON
-     alert("C'est pas un prénom ça !");
-     saisie = prompt("Tape encore ton prénom, stp :");
-     verifName(saisie );
-   }else{
-     //BON
-     alert("Bonjour, "+saisie+" !");
-   }
+  //Expression régulière de vérification
+  const regex =  /^[a-zA-ZäãåçéèêëíìîïñóòôöõúùûüÿÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜŸ-]+$/;
+  //Condition de saisie
+  if(!regex.test(saisie)||saisie==null){
+    //PAS BON
+    alert("C'est pas un prénom ça !");
+    saisie = prompt("Tape encore ton prénom, stp :");
+    verifName(saisie );
+  }else{
+    //BON
+    alert("Bonjour, "+saisie+" !");
+  }
 };
+//exécution de la fonction
 verifName(firstname);
